d6589c2a723358ccded6ddda4a67d9d1bc0a290f
Harry Terkelsen <harry.terkelsen@gmail.com>
2020-02-19 15:34:17 -0800

    Update to Dart 2

--- lib/powerset.dart
+++ lib/powerset.dart
@@ -1,26 +1,23 @@
 // Copyright (c) 2015, Harry Terkelsen. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-/// Generate power sets for any iterable.
-library powerset;
-
 /// Returns the power set of the given [iterable]. This will also work with
 /// infinite iterables. The power set of an iterable is an iterable that
 /// contains all possible subsequences of that iterable.
-Iterable<List> powerset(Iterable iterable) sync* {
+Iterable<List<T>> powerset<T>(Iterable<T> iterable) sync* {
   var flags = <bool>[];
-  var sequence = [];
+  var sequence = <T>[];
   var iterator = iterable.iterator;
 
   while (true) {
-    var subset = [];
-    for (int i = 0; i < flags.length; i++) {
+    var subset = <T>[];
+    for (var i = 0; i < flags.length; i++) {
       if (flags[i]) subset.add(sequence[i]);
     }
     yield subset;
 
     var needsNext = true;
-    for (int i = 0; i < flags.length; i++) {
+    for (var i = 0; i < flags.length; i++) {
       if (!flags[i]) {
         flags[i] = true;
         needsNext = false;
